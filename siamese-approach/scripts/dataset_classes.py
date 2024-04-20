eac6f6136c045f3fc97b2d5e241ff4c9b9a3e82c
opontorno <opontorno@gmail.com>
2024-04-20 20:30:38 +0200

    siamese added

---
+++ siamese-approach/scripts/dataset_classes.py
@@ -0,0 +1,222 @@
+# %% import libraries
+import pandas as pd
+import numpy as np
+import os, random
+from pathlib import Path
+from PIL import Image
+import torch
+import torchvision.transforms as T
+from torch.utils.data import Dataset, Subset
+
+# %%
+class mydataset(Dataset):
+  def __init__(self, dset_dir, guidance, main_class=None, for_basemodel=True, for_testing=False, transforms=T.Compose([])):
+    self.dset_dir = Path(dset_dir)
+    self.transforms = transforms
+    self.files = []
+    labels = pd.read_csv(guidance)
+    models = sorted(os.listdir(self.dset_dir))
+    n=0 if for_basemodel else 1
+    if for_testing==True: n=2
+    for model_name in models:
+      if for_basemodel:
+        assert main_class in models
+        class_idx=1 if model_name==main_class else 0
+      else:
+        class_idx = models.index(model_name) # model class
+      model_path = os.path.join(self.dset_dir, model_name)
+      architectures = sorted(os.listdir(model_path))
+      for architecture_name in architectures:
+        class_idx2 = architectures.index(architecture_name) # architecture class
+        architecture_path = os.path.join(model_path, architecture_name)         
+        for image in os.listdir(architecture_path):
+          image_path = os.path.join(architecture_path, image)
+          if np.array(labels[labels['image_path']==image_path])[0,0]==n:
+            self.files += [{"file": image_path, "class_mod": class_idx, "class_arch": class_idx2}]
+          else:
+            continue
+  def __len__(self):
+    return len(self.files)
+  def __getitem__(self, i):
+    item = self.files[i]
+    file = item['file']
+    class_mod = torch.tensor(item['class_mod'])
+    class_arch = torch.tensor(item['class_arch'])
+    img = Image.open(file).convert("RGB")
+    img = self.transforms(img)
+    return img, class_mod, class_arch
+
+class pair_dset(Dataset):
+  def __init__(self, dset_dir, guidance, main_class=None, for_basemodel=True, for_testing=False, transforms=T.Compose([])):
+     self.dset = mydataset(dset_dir=dset_dir, main_class=main_class, guidance=guidance, for_basemodel=for_basemodel, for_testing=for_testing, transforms=transforms)
+     self.idx_list = get_counts_idx(self.dset, binary=True if for_basemodel else False)[1]
+     self.generate_pairs()
+  def generate_pairs(self):
+     self.pair_labels = (torch.rand(len(self.dset)) > 0.5).long()
+     self.paired_idx = []
+     for idx, label in enumerate(self.pair_labels):
+        c1 = self.dset[idx][1].item()
+        if label==0:
+           j = np.random.choice(self.idx_list[c1])
+        else:
+           diff_class = np.random.choice(list(set(range(len(self.idx_list))) - {c1}))
+           j = np.random.choice(self.idx_list[diff_class])
+        self.paired_idx.append(j)
+  def __len__(self):
+     return(len(self.dset))
+  def __getitem__(self, i):
+     img, klass, _ = self.dset[i]
+     paired_img, paired_klass, _ = self.dset[self.paired_idx[i]]
+     pair_label = self.pair_labels[i]
+     return img, paired_img, klass, paired_klass, pair_label 
+
+class dataset_for_robustness(Dataset):
+  def __init__(self, dset_dir, transforms=T.Compose([])):
+    self.dset_dir = Path(dset_dir)
+    self.transforms = transforms
+    self.files = []
+    models = sorted(os.listdir(self.dset_dir))
+    for model_name in models:
+      class_idx = models.index(model_name) # model class
+      model_path = os.path.join(self.dset_dir, model_name)
+      architectures = sorted(os.listdir(model_path))
+      for architecture_name in architectures:
+        class_idx2 = architectures.index(architecture_name) # architecture class
+        architecture_path = os.path.join(model_path, architecture_name)         
+        for image in os.listdir(architecture_path):
+          image_path = os.path.join(architecture_path, image)
+          self.files += [{"file": image_path, "class_mod": class_idx, "class_arch": class_idx2}]
+  def __len__(self):
+    return len(self.files)
+  def __getitem__(self, i):
+    item = self.files[i]
+    file = item['file']
+    class_mod = torch.tensor(item['class_mod'])
+    class_arch = torch.tensor(item['class_arch'])
+    img = Image.open(file).convert("RGB")
+    img = self.transforms(img)
+    return img, class_mod, class_arch
+    
+class dataset_for_generaization(Dataset):
+  def __init__(self, dset_dir, transforms=T.Compose([])):
+    self.dset_dir = Path(dset_dir)
+    self.transforms = transforms
+    self.files = []
+    classes = sorted(os.listdir(self.dset_dir))
+    for fold in classes:
+      if fold=='0_real': klass=2
+      if fold=='1_fake': klass=1
+      images_path = os.path.join(self.dset_dir, fold)
+      for image in os.listdir(images_path):
+        image_path = os.path.join(images_path, image)
+        self.files += [{"file": image_path, "class_mod": klass}]
+  def __len__(self):
+    return len(self.files)
+  def __getitem__(self, i):
+    item = self.files[i]
+    file = item['file']
+    class_mod = torch.tensor(item['class_mod'])
+    img = Image.open(file).convert("RGB")
+    img = self.transforms(img)
+    return img, class_mod, 1
+
+
+def check_len(dset, binary:bool=False, return_perc=False):
+  print(f'\nlength dataset: {len(dset)}')
+  if not binary:
+    dm, gan,real = 0,0,0
+    for item in dset.files:
+      if item['class_mod']==0: dm += 1 
+      if item['class_mod']==1: gan +=1 
+      if item['class_mod']==2: real+=1
+    perc_dm, perc_gan, perc_real = dm/len(dset), gan/len(dset), real/len(dset)
+    print(f'perc_dms: {perc_dm} \nperc_gans: {perc_gan} \nperc_real: {perc_real}')
+    if return_perc:
+      return perc_dm, perc_gan, perc_real
+  else:
+    main, others = 0,0
+    for item in dset.files:
+      if item['class_mod']==1: main += 1 
+      if item['class_mod']==0: others +=1 
+    perc_main, perc_others = main/len(dset), others/len(dset)
+    print(f'perc_main: {perc_main} \nperc_others: {perc_others}')
+    if return_perc:
+        return perc_main, perc_others
+
+def make_train_valid(dset, validation_ratio=0.2):
+  class_counts = [0, 0, 0]
+  index_lists = [[], [], []]
+  for idx, item in enumerate(dset.files):
+    class_mod = item['class_mod']
+    class_counts[class_mod] += 1
+    index_lists[class_mod].append(idx)
+  min_count = min(class_counts)
+  num_per_class_valid = int(validation_ratio * min_count)
+  valid_indices = []
+  for indices in index_lists:
+    valid_indices.extend(random.sample(indices, num_per_class_valid))
+  all_indices = set(range(len(dset.files)))
+  train_indices = list(all_indices - set(valid_indices))
+  train_dset = Subset(dset, train_indices)
+  valid_dset = Subset(dset, valid_indices)
+  return train_dset, valid_dset
+
+def get_counts_idx(dset, binary=False):
+  class_counts = [0, 0] if binary else [0, 0, 0]
+  index_lists = [[], []] if binary else [[], [], []]
+  for idx, item in enumerate(dset.files):
+    class_mod = item['class_mod']
+    class_counts[class_mod] += 1
+    index_lists[class_mod].append(idx)
+  return class_counts, index_lists
+
+def make_balanced(dset, binary=False):
+    class_counts, index_lists = get_counts_idx(dset, binary=binary)
+    min_count = min(class_counts)
+    _indices = []
+    for indices in index_lists:
+        _indices.extend(random.sample(indices, min_count))
+    _dset = Subset(dset, _indices)
+    return _dset
+
+def balance_binary_test(testing_dset):
+    class_counts = [0, 0]
+    index_lists = [[], []]
+    for idx, item in enumerate(testing_dset.files):
+      if item['class_mod']==0: item['class_mod'] +=1
+      item['class_mod'] -= 1 # 0:deepfake and 1:real
+      class_mod = item['class_mod']
+      class_counts[class_mod] += 1
+      index_lists[class_mod].append(idx)
+    real_count=class_counts[1]
+    binary_test_index = []
+    for indices in index_lists:
+       binary_test_index.extend(random.sample(indices, real_count))
+    test_dset = Subset(testing_dset, binary_test_index)
+    return test_dset
+
+def make_binary(testing_dset):
+  index_list=[]
+  for idx, item in enumerate(testing_dset.files):
+    if item['class_mod']==0: item['class_mod'] +=1
+    item['class_mod'] -= 1 # 0:deepfake and 1:real
+    class_mod = item['class_mod']
+    index_list.append(idx)
+  test_dset = Subset(testing_dset, index_list)
+  return test_dset
+
+def get_trans(model_name:str):
+    if model_name.startswith('vit'):
+        trans = T.Compose([
+            T.Resize((256, 256)),
+            T.CenterCrop((224,224)),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+    else:
+        trans = T.Compose([
+            T.Resize((256, 256)),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+    return trans
---
+++ siamese-approach/scripts/training-complete-model.py
@@ -0,0 +1,71 @@
+# %%
+import torch
+import torch.nn as nn
+import torchvision.transforms as T
+import warnings
+warnings.filterwarnings('ignore')
+from torch.utils.data import random_split, DataLoader
+from torch.optim import Adam
+from torch.optim.lr_scheduler import StepLR
+import sys, os, argparse
+from dataset_classes import *
+from import_classifiers import *
+from training_procedure import *
+from architecture import *
+from settings import *
+
+parser = argparse.ArgumentParser(
+    prog='training complete model',
+    description='train complete model'
+)
+
+parser.add_argument('-b', '--backbone', type=str)
+parser.add_argument('-logs', '--mode_logs', type=str, default='online')
+parser.add_argument('-batch', '--batch_size', type=int, default=32)
+parser.add_argument('-lr', '--learning_rate', type=float, default=1e-4)
+parser.add_argument('-wd', '--weight_decay', type=float, default=1e-3)
+parser.add_argument('-e', '--epochs', type=int, default=50)
+parser.add_argument('-sch', '--scheduler', type=bool, default=False)
+parser.add_argument('-sch_step', '--scheduler_stepsize', type=int, default=10)
+parser.add_argument('-sch_g', '--scheduler_gamma', type=float, default=0.1)
+
+args = parser.parse_args()
+conf = f'-b{args.batch_size}-lr{args.learning_rate}-wd{args.weight_decay}-step{args.scheduler_stepsize}-gamma{args.scheduler_gamma}'
+# %% dataset preparation
+batch_size = args.batch_size
+
+trans = get_trans(model_name=args.backbone)
+    
+dset = mydataset(dset_dir=datasets_path, guidance=guidance_path, for_overfitting=False, for_testing=False, transforms=trans)
+train, valid = make_train_valid(dset=dset, validation_ratio=0.2)
+
+perc_dm, perc_gan, perc_real = check_len(dset, binary=False, return_perc=True)
+
+trainload = DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=False)
+validload = DataLoader(valid, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=False)
+
+loss = nn.CrossEntropyLoss(weight=torch.tensor([1-perc_dm,1-perc_gan,1-perc_real]).to(dev))
+
+# %% training procedure
+backbone_name = args.backbone
+saved_backbone = call_saved_model(backbone_name)
+
+print(f'\n-  {backbone_name}\n')
+model_complete = get_complete_model(backbone_name, models_dir=models_dir)
+
+optimizer = Adam(model_complete.parameters(), 
+                lr=args.learning_rate, 
+                weight_decay=args.weight_decay, 
+                betas=(0.9, 0.999))
+scheduler = StepLR(optimizer=optimizer, step_size=args.scheduler_stepsize, gamma=args.scheduler_gamma) if args.scheduler else None
+
+training(model=model_complete,
+        loaders={'train': trainload, 'valid': validload},
+        epochs=args.epochs,
+        optimizer=optimizer,
+        loss_fn=loss,
+        scheduler=scheduler,
+        mode_logs=args.mode_logs,
+        model_name=backbone_name,
+        save_best_model=True,
+        saving_path=models_dir+'/grid_complete_models/'+saved_backbone+conf+'.pt')
